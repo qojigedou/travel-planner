352f454eed27eeb36fb4eaa84cc6b7a5b91db597
manulenka <viktoryia.manulenka@student.uj.edu.pl>
2026-09-12 22:14:46 +0200

    modify how to get postgresql

--- app/backend/database/session_postgresql.py
+++ app/backend/database/session_postgresql.py
@@ -2,9 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from typing import Generator
 import os
+from dotenv import load_dotenv
 from contextlib import contextmanager
 
+settings = get_settings()
+
 POSTGRESQL_DB_URL = os.getenv("POSTGRESQL_DB_URL")
+
+if not POSTGRESQL_DB_URL:
+    raise RuntimeError("POSTGRESQL_DB_URL is not set")
+
 postgres_engine = create_engine(POSTGRESQL_DB_URL)
 PostgresqlSessionLocal = sessionmaker(
     autocommit=False,
